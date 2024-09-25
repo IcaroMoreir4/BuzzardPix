638864e5ffd55fca78a785a9a5b4a75994b6f54f
Ícaro Moreira <icaromoreira91@gmail.com>
2024-09-25 12:08:16 -0300

    consertando cores no login

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,10 @@ document.getElementById('abrirMenu').addEventListener('click', function() {
 document.getElementById('fecharMenu').addEventListener('click', function() {
     document.getElementById('menu').style.left = '-100%';  // Esconde o menu ao clicar
 });
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    if (!this.checkValidity()) {
+        event.preventDefault(); 
+        alert('Por favor, preencha todos os campos corretamente.'); 
+    }
+});
