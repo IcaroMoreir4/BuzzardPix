9b30fb7e3ddeb4360a77e99d84636a8a3f10c80c
Ícaro Moreira <icaromoreira91@gmail.com>
2024-10-17 16:25:56 -0300

    login e registro

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'cor-0': 'var(--cor-0)',
-        'cor-1': 'var(--cor-1)',
-        'cor-2': 'var(--cor-2)',
-        'cor-3': 'var(--cor-3)',
-        'cor-4': 'var(--cor-4)',
-        'cor-5': 'var(--cor-5)',
-        'cor-6': 'var(--cor-6)',
-        'cor-7': 'var(--cor-7)',
-        'cor-8': 'var(--cor-8)',
-        'cor-9': 'var(--cor-9)',
-        'cor-10': 'var(--cor-10)',
-        'cor-11': 'var(--cor-11)',
-        'cor-12': 'var(--cor-12)',
-        'cor-p1': 'var(--cor-p1)',
-        'cor-p2': 'var(--cor-p2)',
+        "bzd-orange-light": "#f09819",
+        "bzd-orange": "#fe6501",
+        "bzd-orange-dark": "#831b0b",
+        "bzd-white": "#ffffff",
+        "bzd-gray": "#f1eded",
+        "bzd-black": "#000000",
       },
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
